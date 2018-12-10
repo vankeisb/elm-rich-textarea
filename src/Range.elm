b760ac66404b81017b17521bb58d34ac837a8765
Rémi Van Keisbelck <remi@rvkb.com>
2018-12-10 14:27:05 +0100

    API doc

--- src/Range.elm
+++ src/Range.elm
@@ -12,10 +12,19 @@ module Range exposing
     )
 
 
+{-| Ranges "from" -> "to".
+
+-}
+
+
+{-| Range opaque type.
+-}
 type Range
     = Range Int Int
 
 
+{-| Create a range
+-}
 range : Int -> Int -> Range
 range from to =
     if from < to then
@@ -25,6 +34,7 @@ range from to =
         Range to from
 
 
+{-| Expand passed range -}
 expand : Int -> Int -> Range
 expand to at =
     if to <= at then
@@ -34,44 +44,45 @@ expand to at =
         Range at to
 
 
+{-| Return true if range contains passed index -}
 contains : Int -> Range -> Bool
 contains index (Range from to) =
     (index >= from) && (index < to)
 
 
 
--- TODO remplace getters by map
-
-
+{-| Return (from,to) for passed range -}
 getBounds : Range -> ( Int, Int )
 getBounds (Range from to) =
     ( from, to )
 
 
 
--- TODO remplace getters by map
-
-
+{-| Return "from" bound for the range -}
 getFrom : Range -> Int
 getFrom (Range from _) =
     from
 
 
+{-| Return true if the range is a caret at passed index -}
 isCaret : Int -> Range -> Bool
 isCaret i (Range from to) =
     (from == to) && (from == i)
 
 
+{-| Determines if range is empty (width == 0) -}
 empty : Range -> Bool
 empty (Range from to) =
     from == to
 
 
+{-| Move a Range with passed int -}
 move : Int -> Range -> Range
 move i (Range from to) =
     Range (from + i) (to + i)
 
 
+{-| Insert count at position -}
 insertAt : Int -> Int -> Range -> Range
 insertAt pos count (Range from to) =
     if pos < from then
